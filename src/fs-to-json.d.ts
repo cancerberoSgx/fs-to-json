39ec2db86aa2c2f4c672aef0c74556b5cb752408
Sebastián Gurín <sgurin@awalabs.com>
2019-08-25 23:58:11 -0300

    0.2.0

--- src/fs-to-json.d.ts
+++ src/fs-to-json.d.ts
@@ -1,12 +1,10 @@
-declare module 'fs-to-json' {
-  export interface Config {
-    input: string,
-    output?: string | { write: (...arg: any[]) => any }
-    transformFileName?: (string) => string
-    filenamePropertyName?: string
-    contentPropertyName?: string
-    formatted?: boolean,
-    outputStyle?: 'object' | 'array'
-  }
-  export function fs2json(config: Config): Promise<any>
+export interface Config {
+  input: string,
+  output?: string | { write: (...arg: any[]) => any }
+  transformFileName?: (s: string) => string
+  filenamePropertyName?: string
+  contentPropertyName?: string
+  formatted?: boolean,
+  outputStyle?: 'object' | 'array'
 }
+export function fs2json(config: Config): Promise<any>
